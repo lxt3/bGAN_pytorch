3e576c015ae3521906ab859eeaa0abb6314e8246
lxt3 <laura.thompson@fda.hhs.gov>
2025-11-18 21:12:08 -0500

    Resolved user warning about deprecation of size_average and reduce args in loss function

--- tabular_data/ComplementCrossEntropyLoss.py
+++ tabular_data/ComplementCrossEntropyLoss.py
@@ -10,13 +10,15 @@ def _assert_no_grad(variable):
 class ComplementCrossEntropyLoss(torch.nn.Module):
   # Note: This is the cross entropy of the sum of all probabilities of other indices, except for the 
   # This is used in Bayesian GAN semi-supervised learning
-  def __init__(self, except_index=None, weight=None, ignore_index=-100, size_average=True, reduce=True):
+
+  # FIX: REMOVED size_average=True and reduce=True from the signature
+  def __init__(self, except_index=None, weight=None, ignore_index=-100): #, size_average=True, reduce=True):
     super(ComplementCrossEntropyLoss, self).__init__()
     self.except_index = except_index
     self.weight = weight
     self.ignore_index = ignore_index
-    self.size_average = size_average
-    self.reduce = reduce
+  #  self.size_average = size_average
+  #  self.reduce = reduce
     
   def forward(self, input, target=None):     #  def forward(self, input, target=None, weight=None):
     if target is not None:  # Use target if not None, else use self.except_index
@@ -27,6 +29,7 @@ class ComplementCrossEntropyLoss(torch.nn.Module):
     result = torch.nn.functional.nll_loss(
       torch.log(1. - torch.nn.functional.softmax(input, dim=1) + 1e-4), 
       target, weight = self.weight, #weight if weight is not None else self.weight,  # Use the provided weight or the default #self.weight, 
-      size_average=self.size_average, 
+    #  size_average=self.size_average, 
+      reduction='mean', # to handle warning
       ignore_index=self.ignore_index)
     return result
